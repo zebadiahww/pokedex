2099ed2005a856ef578387c8c9271335b5ca016e
Zebadiah <zebadiahww@gmail.com>
2019-10-01 12:38:55 -0600

    project complete

---
+++ Pokedex/Controller/Model Controller/PokemonController.swift
@@ -0,0 +1,9 @@
+//
+//  PokemonController.swift
+//  Pokedex
+//
+//  Created by Zebadiah Watson on 10/1/19.
+//  Copyright © 2019 Zebadiah Watson. All rights reserved.
+//
+
+import Foundation
---
+++ Pokedex/Controller/View Controller/PokemonViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PokemonViewController.swift
+//  Pokedex
+//
+//  Created by Zebadiah Watson on 10/1/19.
+//  Copyright © 2019 Zebadiah Watson. All rights reserved.
+//
+
+import UIKit
+
+class PokemonViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
